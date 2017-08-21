5bb8d4b8b7251ebecb929315412261cc73a408e1
Ryan Bregier <rabunc@gmail.com>
2017-08-21 19:22:42 -0400

    routes added to server.js

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
