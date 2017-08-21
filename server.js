3d3a6055c25e8358ddf70d32922f4a7df4e6251f
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-21 19:45:16 -0400

    added app folder

--- server.js
+++ server.js
@@ -12,12 +12,18 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public")); // virtual path
+// can also provide a specific virtual path
+// example: app.use("/jim", express.static("app/public"));
 
 // listening on port
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
+require("./data/reservationList.js");
+require("./data/waitList.js");
+
+// html routes
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -28,6 +34,16 @@ app.get("/tables", function(req, res) {
 
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/reserve.html"));
+});
+
+
+// API data routes
+app.get("/api/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "/data/reservationList.js"));
+});
+
+app.get("/api/waitlist", function(req, res) {
+  res.sendFile(path.join(__dirname, "/data/waitList.js"));
 });
 
 app.post("/api/new",function(req, res) {
